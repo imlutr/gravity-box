491ab0a87f6ce005f08df6797566f6dcc7b9d2bc
Luca1152 <t.luca1152@gmail.com>
2018-09-07 13:06:42 +0300

    Initial commit.

---
+++ android/src/ro/luca1152/gravitybox/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ro.luca1152.gravitybox;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ro.luca1152.gravitybox.MyGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGame(), config);
+	}
+}
