31f6cdaab6285bcd9b4f474ad272cba2cc20bc6c
Luca1152 <t.luca1152@gmail.com>
2019-02-04 20:46:44 +0200

    Extended the Button class so I can shorten code.

---
+++ core/src/ro/luca1152/gravitybox/utils/MyButton.kt
@@ -0,0 +1,55 @@
+/*
+ * This file is part of Gravity Box.
+ *
+ * Gravity Box is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gravity Box is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gravity Box.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ro.luca1152.gravitybox.utils
+
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.ui.Button
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+
+class MyButton(skin: Skin, styleName: String, drawableName: String = "",
+               upColor: Color = Color.WHITE, downColor: Color = Color.WHITE,
+               touchUpRunnable: Runnable? = null) : Button(skin, styleName) {
+    val image = if (drawableName != "") Image(skin, drawableName) else null
+
+    init {
+        if (image != null)
+            add(image)
+
+        color = upColor
+        image?.color = upColor
+
+        addListener(object : ClickListener() {
+            override fun touchDown(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int): Boolean {
+                color = downColor
+                image?.color = downColor
+                return true
+            }
+
+            override fun touchUp(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int) {
+                color = upColor
+                image?.color = upColor
+
+                if (isOver(this@MyButton, x, y))
+                    touchUpRunnable?.run()
+            }
+        })
+    }
+}
