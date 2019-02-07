3ca155f31d610f7ee1a0a2288969fbaa57c14429
Luca1152 <t.luca1152@gmail.com>
2019-02-07 21:58:40 +0200

    Added an initial structure to the level editor backend.

---
+++ core/src/ro/luca1152/gravitybox/utils/map/objects/PlatformObject.kt
@@ -0,0 +1,38 @@
+/*
+ * This file is part of Gravity Box.
+ *
+ * Gravity Box is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gravity Box is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gravity Box.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ro.luca1152.gravitybox.utils.map.objects
+
+import com.badlogic.gdx.math.Vector2
+import ro.luca1152.gravitybox.utils.map.Object
+
+class PlatformObject(id: Int,
+                     val position: Vector2,
+                     var type: PlatformType,
+                     var width: Float, var height: Float,
+                     var botLeftCorner: CornerType, var topLeftCorner: CornerType,
+                     var botRightCorner: CornerType, var topRightCorner: CornerType) : Object(id)
+
+enum class PlatformType {
+    FULL, // Full interior, can be destroyed
+    EMPTY // Empty interior, can be destroyed
+}
+
+enum class CornerType {
+    ROUNDED,
+    STRAIGHT
+}
