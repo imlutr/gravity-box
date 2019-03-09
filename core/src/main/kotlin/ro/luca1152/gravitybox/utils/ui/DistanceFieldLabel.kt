f8a6abeb9ba2d45ae2ccb42968e6fbfcf8a809f5
Luca1152 <t.luca1152@gmail.com>
2019-03-09 15:58:49 +0200

    Fix distance field shader
    The shader I used (the one from `DistanceFieldFont.createDistanceFieldShader()`) didn't work, and the distance field text looked the same as it did without the shader.

--- core/src/main/kotlin/ro/luca1152/gravitybox/utils/ui/DistanceFieldLabel.kt
+++ core/src/main/kotlin/ro/luca1152/gravitybox/utils/ui/DistanceFieldLabel.kt
@@ -19,24 +19,69 @@
 
 package ro.luca1152.gravitybox.utils.ui
 
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.Batch
-import com.badlogic.gdx.graphics.g2d.DistanceFieldFont
+import com.badlogic.gdx.graphics.glutils.ShaderProgram
 import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 
 class DistanceFieldLabel(
     text: CharSequence,
     skin: Skin,
-    styleName: String
-) : Label(text, skin, styleName) {
+    styleName: String,
+    fontSize: Float = 32f,
+    color: Color = Color.WHITE
+) : Label(text, skin, styleName, Color.WHITE) {
     companion object {
-        private val distanceFieldShader = DistanceFieldFont.createDistanceFieldShader()
+        private val vertexShader = """
+            uniform mat4 u_projTrans;
+
+            attribute vec4 a_position;
+            attribute vec2 a_texCoord0;
+            attribute vec4 a_color;
+
+            varying vec4 v_color;
+            varying vec2 v_texCoord;
+
+            void main() {
+                gl_Position = u_projTrans * a_position;
+                v_texCoord = a_texCoord0;
+                v_color = a_color;
+            }
+        """.trimIndent()
+
+        private val fragmentShader = """
+            #ifdef GL_ES
+            precision mediump float;
+            #endif
+
+            uniform sampler2D u_texture;
+
+            varying vec4 v_color;
+            varying vec2 v_texCoord;
+
+            const float smoothing = 1.0/16.0;
+
+            void main() {
+                float distance = texture2D(u_texture, v_texCoord).a;
+                float alpha = smoothstep(0.5 - smoothing, 0.5 + smoothing, distance);
+                gl_FragColor = vec4(v_color.rgb, v_color.a * alpha);
+            }
+        """.trimIndent()
+
+        private val distanceFieldShader = ShaderProgram(vertexShader, fragmentShader)
+
+        private const val DEFAULT_FONT_SIZE = 32f
+    }
+
+    init {
+        this.color = color
+        setFontScale(fontSize / DEFAULT_FONT_SIZE)
     }
 
     override fun draw(batch: Batch, parentAlpha: Float) {
-        val oldShader = batch.shader
         batch.shader = distanceFieldShader
         super.draw(batch, parentAlpha)
-        batch.shader = oldShader
+        batch.shader = null
     }
 }
