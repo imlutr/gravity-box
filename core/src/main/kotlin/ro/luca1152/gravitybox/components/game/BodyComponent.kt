6c9235e0e6ec39628e867b3046d12f37e944878f
Luca1152 <t.luca1152@gmail.com>
2019-03-23 18:15:10 +0200

    Fix bodies being created twice
    In the BodyComponent, the `body` variable had the initial value set as `world.createBody(...)`, but in the `set()` method, where the actual body is set, the old body wasn't deleted.

--- core/src/main/kotlin/ro/luca1152/gravitybox/components/game/BodyComponent.kt
+++ core/src/main/kotlin/ro/luca1152/gravitybox/components/game/BodyComponent.kt
@@ -23,7 +23,6 @@ import com.badlogic.gdx.physics.box2d.Body
 import com.badlogic.gdx.physics.box2d.BodyDef
 import com.badlogic.gdx.physics.box2d.World
 import com.badlogic.gdx.utils.Pool.Poolable
-import com.badlogic.gdx.utils.Pools
 import ro.luca1152.gravitybox.utils.box2d.EntityCategory
 import ro.luca1152.gravitybox.utils.components.ComponentResolver
 import ro.luca1152.gravitybox.utils.kotlin.bodies
@@ -43,10 +42,7 @@ class BodyComponent(private val world: World = Injekt.get()) : Component, Poolab
     var initialY = Float.POSITIVE_INFINITY
     var initialRotationRad = 0f
 
-    private val bodyDefToDispose = Pools.obtain(BodyDef::class.java)
-    var body: Body = world.createBody(bodyDefToDispose).apply {
-        Pools.free(bodyDefToDispose)
-    }
+    lateinit var body: Body
 
     fun set(
         body: Body, userData: Entity,
