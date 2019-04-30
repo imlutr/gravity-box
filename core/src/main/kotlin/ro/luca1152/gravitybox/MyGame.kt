a1f8c923b05537a5eb71db98f16c9ac1e9cf111e
Luca1152 <t.luca1152@gmail.com>
2019-04-30 20:51:54 +0300

    Add levels
    There are now 100 levels.

--- core/src/main/kotlin/ro/luca1152/gravitybox/MyGame.kt
+++ core/src/main/kotlin/ro/luca1152/gravitybox/MyGame.kt
@@ -38,7 +38,7 @@ import ro.luca1152.gravitybox.utils.kotlin.*
 /** The main class of the game. */
 class MyGame : KtxGame<Screen>() {
     companion object {
-        const val LEVELS_NUMBER = 78
+        const val LEVELS_NUMBER = 100
     }
 
     private val context = Context()
