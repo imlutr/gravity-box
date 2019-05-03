3cc02d3e58ae063b7a8d182f12380c76d877e448
Luca1152 <t.luca1152@gmail.com>
2019-05-04 00:10:32 +0300

    Fix colors
    If the player spawned in the finish point (and the map had collectible points) the color would still lerp a bit, even though it wasn't supposed to.

--- core/src/main/kotlin/ro/luca1152/gravitybox/systems/game/ColorSchemeSystem.kt
+++ core/src/main/kotlin/ro/luca1152/gravitybox/systems/game/ColorSchemeSystem.kt
@@ -21,8 +21,8 @@ import com.badlogic.ashley.core.Engine
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.EntitySystem
 import ro.luca1152.gravitybox.components.game.FinishComponent
+import ro.luca1152.gravitybox.components.game.LevelComponent
 import ro.luca1152.gravitybox.components.game.PlayerComponent
-import ro.luca1152.gravitybox.components.game.collisionBox
 import ro.luca1152.gravitybox.utils.kotlin.approxEqualTo
 import ro.luca1152.gravitybox.utils.kotlin.getSingleton
 import ro.luca1152.gravitybox.utils.ui.Colors
@@ -31,19 +31,17 @@ import ro.luca1152.gravitybox.utils.ui.Colors
 class ColorSchemeSystem : EntitySystem() {
     private lateinit var playerEntity: Entity
     private lateinit var finishEntity: Entity
+    private lateinit var levelEntity: Entity
     private var generatedNewColors = false
-    private val playerIsInsideFinishPoint
-        get() = playerEntity.collisionBox.box.overlaps(finishEntity.collisionBox.box)
 
     override fun addedToEngine(engine: Engine) {
         playerEntity = engine.getSingleton<PlayerComponent>()
         finishEntity = engine.getSingleton<FinishComponent>()
+        levelEntity = engine.getSingleton<LevelComponent>()
     }
 
     override fun update(deltaTime: Float) {
-        if (!Colors.isDirty || playerIsInsideFinishPoint) {
-            return
-        }
+        if (!Colors.isDirty) return
         lerpToNewHue()
     }
 
@@ -53,16 +51,14 @@ class ColorSchemeSystem : EntitySystem() {
     }
 
     private fun updateNewColors() {
-        if (generatedNewColors) {
-            return
-        }
+        if (generatedNewColors) return
         Colors.LightTheme.resetAllColors(Colors.hue)
         Colors.DarkTheme.resetAllColors(Colors.hue)
         generatedNewColors = true
     }
 
     private fun lerpColors() {
-        Colors.lerpTowardsDefaultColors(.015f)
+        Colors.lerpTowardsDefaultColors(.05f)
         if (Colors.uiDownColor.approxEqualTo(Colors.LightTheme.game29)) {
             Colors.isDirty = false
             generatedNewColors = false
