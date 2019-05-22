8fd95586724de60e71be84c89a7f9453e5ebb333
Luca1152 <t.luca1152@gmail.com>
2019-05-22 20:19:57 +0300

    Add levels
    There are now 270 levels.

--- core/src/main/kotlin/ro/luca1152/gravitybox/MyGame.kt
+++ core/src/main/kotlin/ro/luca1152/gravitybox/MyGame.kt
@@ -39,7 +39,7 @@ import ro.luca1152.gravitybox.utils.ui.DistanceFieldLabel
 /** The main class of the game. */
 class MyGame : KtxGame<Screen>() {
     companion object {
-        const val LEVELS_NUMBER = 266
+        const val LEVELS_NUMBER = 270
     }
 
     private val context = Context()
