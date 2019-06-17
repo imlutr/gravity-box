c6131101048f36d5a5c58810c8b4a95ee59b8dfb
Luca1152 <t.luca1152@gmail.com>
2019-06-17 13:08:06 +0300

    Decrease max interstitial ads
    The maximum number of interstitial ads per session is now 4, instead of 5.

--- core/src/main/kotlin/ro/luca1152/gravitybox/GameRules.kt
+++ core/src/main/kotlin/ro/luca1152/gravitybox/GameRules.kt
@@ -171,7 +171,7 @@ class GameRules(context: Context) {
     /** There must be a delay of at least 2 minutes between two interstitial ads. */
     val TIME_DELAY_BETWEEN_INTERSTITIAL_ADS = 2f * 60
     /** A maximum of 5 interstitial ads should be shown in one session (until the game is closed). */
-    val MAX_INTERSTITIAL_ADS_PER_SESSION = 5
+    val MAX_INTERSTITIAL_ADS_PER_SESSION = 4
     /**
      * True when an ad should be shown.
      * Set true by the InterstitialAdsSystem.
