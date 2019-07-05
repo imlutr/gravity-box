6baedf69f3c64a93c23601c389f7fbc0469e39de
Luca1152 <t.luca1152@gmail.com>
2019-07-05 15:20:41 +0300

    Rearrange database actions
    I was using an 'after' previously. Now I don't use it anymore, I use only 'then's. Also, I removed all `silentFail()`s, as I now set a global property once which does the same thing.
    
    Also, I added Firebase logs (back), so I know what happened before a crash.

--- core/src/main/kotlin/ro/luca1152/gravitybox/utils/leaderboards/ShotsLeaderboard.kt
+++ core/src/main/kotlin/ro/luca1152/gravitybox/utils/leaderboards/ShotsLeaderboard.kt
@@ -19,6 +19,7 @@ package ro.luca1152.gravitybox.utils.leaderboards
 
 import ktx.inject.Context
 import pl.mk5.gdx.fireapp.GdxFIRAuth
+import pl.mk5.gdx.fireapp.GdxFIRCrash
 import pl.mk5.gdx.fireapp.GdxFIRDatabase
 import pl.mk5.gdx.fireapp.auth.GdxFirebaseUser
 import ro.luca1152.gravitybox.GameRules
@@ -34,26 +35,25 @@ class ShotsLeaderboard(context: Context) {
         }
         val databasePath = "shots-leaderboard/game/${gameRules.GAME_LEVELS_VERSION}/l$level/s$shots"
         GdxFIRAuth.inst().signInAnonymously().then<GdxFirebaseUser> {
-            GdxFIRDatabase.inst()
-                // Update the player count for the given number of shots
-                .inReference(databasePath).transaction(java.lang.Long::class.java) { (it.toLong() + increment) as java.lang.Long }
-                .silentFail()
-                .then(GdxFIRDatabase.inst().inReference(databasePath).readValue(java.lang.Long::class.java))
-                .silentFail()
-                // If the player count is 0 (or, for some reason, negative), delete the entry
-                .then<java.lang.Long> {
-                    if (increment < 0 && it <= 0) {
-                        GdxFIRDatabase.inst().inReference(databasePath).removeValue().silentFail()
-                    }
-                    // Do all of the above only after you made sure that the databasePath really points to an entry; if it doesn't, create one
-                }.after(
-                    GdxFIRDatabase.inst().inReference(databasePath).readValue(java.lang.Long::class.java).then<java.lang.Long> {
-                        if (it == null) {
-                            GdxFIRDatabase.inst().inReference(databasePath).setValue(1).silentFail()
-                        }
-                    }
-                ).silentFail()
-        }.silentFail()
+            GdxFIRCrash.inst().log("Signing in anonymously into Firebase")
+            GdxFIRDatabase.inst().inReference(databasePath).readValue(java.lang.Long::class.java).then<java.lang.Long> {
+                if (it == null) {
+                    GdxFIRCrash.inst().log("Setting the value at $databasePath to 0")
+                    GdxFIRDatabase.inst().inReference(databasePath).setValue(0)
+                }
+            }.then<java.lang.Long> {
+                GdxFIRDatabase.inst().inReference(databasePath).transaction(java.lang.Long::class.java) { value ->
+                    GdxFIRCrash.inst()
+                        .log("Incrementing the value at $databasePath by $increment ($value becomes ${value.toLong() + increment})")
+                    (value.toLong() + increment) as java.lang.Long
+                }
+            }.then<java.lang.Long> {
+                if (increment < 0 && (it != null && it.toLong() + increment <= 0)) {
+                    GdxFIRCrash.inst().log("Removing the value at $databasePath")
+                    GdxFIRDatabase.inst().inReference(databasePath).removeValue()
+                }
+            }
+        }
     }
 
     fun incrementPlayerCountForShots(level: Int, shots: Int) {
